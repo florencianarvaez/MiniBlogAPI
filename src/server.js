29055234ee7fd5dafbe9466f56c26b561de4d5f6
Florencia <florencianarvaez99@gmail.com>
2026-07-01 09:50:04 -0300

    fix: usar PORT de Railway

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+

56677616de4efa1c812e2dca0e0787c0d385b561
Florencia <florencianarvaez99@gmail.com>
2026-07-01 08:31:13 -0300

    Configura host para Railway

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
+const HOST = "0.0.0.0";
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });

260fc3c95ba48a18c20485a2634d6ae64592bee2
Florencia <florencianarvaez99@gmail.com>
2026-07-01 08:16:30 -0300

    Usa DATABASE_URL para Railway

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,13 +1,20 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
+      }
+);
 
 module.exports = pool;
+
 
